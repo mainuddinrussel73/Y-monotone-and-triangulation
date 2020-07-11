2e7a849ab2118d07ed3ce5c1c58eb66a26fdc823
main <mainuddinrusselbuet73@yahoo.com>
2020-07-11 11:36:24 +0600

    ok
    done

--- Polygon.java
+++ Polygon.java
@@ -4,8 +4,9 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 
 class point {
+    int index;
     double X;double Y;
-    point(double x,double y){ this.Y = y;this.X = x; }
+    point(double x,double y,int index){ this.Y = y;this.X = x;this.index = index; }
     @Override public String toString() {
         return X+" "+Y;
     }
@@ -15,14 +16,7 @@ class vertex{
     double a, b, c;
 
     vertex(point p1){ data = p1; }
-	public double calculatedistance(polygon p){
-		
-		double distance = 0;
-        for (int i = 0; i < verteces.size(); i++) {
-            distance  += (verteces.get((i+1)%verteces.size()).data.X - verteces.get(i).data.X)* (verteces.get((i+1)%verteces.size()).data.Y + verteces.get(i).data.Y);
-        }
-        return distance ;
-    }
+
     public String getvertextype(polygon p,int index) {
         point p_cur = p.verteces.get(index).data;
         point p_prev;
@@ -50,7 +44,7 @@ class vertex{
         else return false;
 
     }
-    public void  sweepline(vertex p1, vertex p2) {
+    public void  line(vertex p1, vertex p2) {
         this.a = p1.data.Y- p2.data.Y;
         this.b = p2.data.X - p1.data.X;
         this.c = p1.data.X*p2.data.Y - p2.data.X*p1.data.Y;
@@ -68,17 +62,16 @@ class edge{
         v_i = v1;
         v_i_1 = v2;
     }
-    public boolean intersectswap(double sweeep,polygon p,int index) {
-        if(sweeep >= p.verteces.get((index+1)%p.verteces.size()).data.Y){
-        	 if(sweepY <= p.verteces.get(index).data.Y)return true;
-        }return false;
+    public boolean intersectswap(double sweepY,polygon p,int index) {
+        return (sweepY >= p.verteces.get(index).data.Y && sweepY <= p.verteces.get((index+1)%p.verteces.size()).data.Y) ||
+                (sweepY >= p.verteces.get((index+1)%p.verteces.size()).data.Y && sweepY <= p.verteces.get(index).data.Y);
     }
     public boolean rightside(polygon p ,int index, edge e, double Y) {
-        p.verteces.get(index).sweepline(p.verteces.get(index),p.verteces.get((index+1)%p.verteces.size()));
+        p.verteces.get(index).line(p.verteces.get(index),p.verteces.get((index+1)%p.verteces.size()));
         return p.verteces.get(index).xvalue(Y) > p.edges.get(p.edges.indexOf(e)).v_i.xvalue(Y);
     }
     public boolean leftside(polygon p ,int index,vertex v) {
-        p.verteces.get(index).sweepline(p.verteces.get(index),p.verteces.get((index+1)%p.verteces.size()));
+        p.verteces.get(index).line(p.verteces.get(index),p.verteces.get((index+1)%p.verteces.size()));
         return p.verteces.get(index).xvalue(v.data.Y) < v.data.X;
     }
 }
@@ -97,59 +90,72 @@ class polygon {
             this.edges.add(e);
         }
     }
-	 
+
     public void makecounterclockwise() {
-       
-		if(calculatedistance(this) <0) return ;
-        vertex temp;
-        for (int i = 1; i <= verteces.size() / 2; i++) {
-        	temp = verteces.get(i);
-            verteces.set(i, verteces.get(verteces.size()-i));
-            verteces.set(verteces.size()-i, temp);
+
+        double sum = 0;
+        for (int i = 0; i < verteces.size(); i++) {
+            sum += (verteces.get((i+1)%verteces.size()).data.X - verteces.get(i).data.X)*
+                    (verteces.get((i+1)%verteces.size()).data.Y + verteces.get(i).data.Y);
         }
-        
+
+        if (sum>0) {
+            vertex temp;
+            for (int i = 1; i <= verteces.size() / 2; i++) {
+                temp = verteces.get(i);
+                verteces.set(i, verteces.get(verteces.size()-i));
+                verteces.set(verteces.size()-i, temp);
+            }
+        }
     }
 
     public boolean isconvex(polygon p,int i) {
-        int i_1 = i - 1;
-        if (i_1< 0) i_1+= p.verteces.size();
-        int i_i_1= (i + 1) % p.verteces.size();
-        boolean b=false;
-		 if((p.verteces.get(i).data.X - .p.verteces.get(i_1).data.X) * (p.verteces.get(i_i_1).data.Y - p.verteces.get(i_1).data.Y) - (p.verteces.get(i_i_1).data.X - p.verteces.get(i_1).data.X) * (p.verteces.get(i).data.Y -p.verteces.get(i_1).data .Y)>0) b=true;
-		 else b=false;
-        return b;
+        int prev = i - 1;
+        if (prev < 0) prev += p.verteces.size();
+        int next = (i + 1) % p.verteces.size();
+
+        if (leftturn(p.verteces.get(prev).data, p.verteces.get(i).data, p.verteces.get(next).data)){
+            return true;
+        }return false;
+    }
+
+    boolean leftturn(point p0, point p1, point p2) {
+        if((p1.X - p0.X) * (p2.Y - p0.Y) - (p2.X - p0.X) * (p1.Y - p0.Y)>0)return true;
+        else return false;
     }
 
     Pair<Integer,Integer> findtoplow(polygon p){
-    	point top;
-        point bottom  =  top = p.verteces.get(0).data;
-        int k =0;
-        int j = 0;
+        point Bottom  = p.verteces.get(0).data;
+        point Top = p.verteces.get(0).data;
+        int top_idx =0;
+        int bot_idx = 0;
         for (int i = 0; i < p.verteces.size(); i++) {
             point temp = p.verteces.get(i).data;
-            if (bottom .Y < temp.Y) {
-                bottom = temp;
-                k = i;
+            if (Top.Y < temp.Y) {
+                Top = temp;
+                top_idx = i;
             }
-            else if (top.Y > temp.Y) {
-                top = temp;
-                j = i;
+            if (Bottom.Y > temp.Y) {
+                Bottom = temp;
+                bot_idx = i;
             }
         }
-        return new Pair<>(new Integer(k),new Integer(j));
+        return new Pair<>(new Integer(top_idx),new Integer(bot_idx));
     }
-    void partition(polygon p,int k,int j){
-        int i = j;
-        for (i=0;i <= j;i++) {
+    void partition(polygon p,int top_idx,int bot_idx){
+        int i = top_idx;
+        while (i != bot_idx) {
             leftchain.add(p.verteces.get(i));
+            i++;
             if(i>=p.verteces.size()) i %= p.verteces.size();
         }
-       
-        i = k - 1 + p.verteces.size();
+        leftchain.add(p.verteces.get(i));
+        i = top_idx - 1 + p.verteces.size();
         if(i>=p.verteces.size())i %= p.verteces.size();
 
-        for (i=0;i<= k;i=-1+p.verteces.get(i)){
+        while (i != bot_idx) {
             rightchain.add(p.verteces.get(i));
+            i = i - 1 + p.verteces.size();
             if(i>=p.verteces.size())i %= p.verteces.size();
         }
     }
@@ -158,42 +164,40 @@ class polygon {
 
 
         Pair<Integer,Integer> pair = findtoplow(p);
-        int top = pair.getKey().intValue();
-        int bottom  = pair.getValue().intValue();
-        partition(p,top,bottom );
+        int top_idx = pair.getKey().intValue();
+        int bot_idx = pair.getValue().intValue();
+        partition(p,top_idx,bot_idx);
         int i = 0;
         int j = 0;
 
         while (i < leftchain.size() || j < rightchain.size()) {
+            if (i >= leftchain.size()) {
+                sortedlist.add(rightchain.get(j));
+                j++;
+            } else if (j >= rightchain.size()) {
+                sortedlist.add(leftchain.get(i));
+                i++;
+            } else {
+                point a, b;
+                a = leftchain.get(i).data;
+                b = rightchain.get(j).data;
 
-                if(i < leftchain.size() && j < rightchain.size()){
-                    point a, b;
-                    a = leftchain.get(i).data;
-                    b = rightchain.get(j).data;
-                    if (a.Y > b.Y) {
-                        sortedlist.add(leftchain.get(i));
-                        i++;
-                    } else if (b.Y > a.Y) {
-                        sortedlist.add(rightchain.get(j));
-                        j++;
-                    } else {
-                        if (a.X < b.X) {
-                            sortedlist.add(leftchain.get(i));
-                            i++;
-                        } else {
-                            sortedlist.add(rightchain.get(j));
-                            j++;
-                        }
-                    }
-                }
-                else if (j >= rightchain.size()) {
+                if (a.Y > b.Y) {
                     sortedlist.add(leftchain.get(i));
                     i++;
-                }
-                else if (i >= leftchain.size()) {
+                } else if (b.Y > a.Y) {
                     sortedlist.add(rightchain.get(j));
                     j++;
+                } else {
+                    if (a.X < b.X) {
+                        sortedlist.add(leftchain.get(i));
+                        i++;
+                    } else {
+                        sortedlist.add(rightchain.get(j));
+                        j++;
+                    }
                 }
+            }
         }
 
     }
